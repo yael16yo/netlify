927b27a32bb869f86805a427ca6a741ecac00517
yael16yo <75533817+yael16yo@users.noreply.github.com>
2024-09-12 12:45:45 +0200

    update(cors)

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,25 @@
-require('dotenv').config();
-const express = require("express")
-const app = express()
-const cors = require('cors')
+require('dotenv').config(); // Importer dotenv pour charger les variables d'environnement
 
-const routeMails = require('./routes/routeMails.js')
+const express = require("express");
+const app = express();
+const cors = require('cors');
 
+const routeMails = require('./routes/routeMails.js');
 
+// Configuration de CORS et JSON
 const corsOptions = {
-    origin: '*', // This allows requests from any origin (equivalent to Access-Control-Allow-Origin: *)
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Allowed methods
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content', 'Accept', 'Content-Type', 'Authorization'], // Allowed headers
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content', 'Accept', 'Content-Type', 'Authorization'],
 };
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use('/api/mails', routeMails)
+
+// Utiliser les routes
+app.use('/api/mails', routeMails);
+
+const PORT = process.env.PORT || 6284; // Utiliser la variable d'environnement PORT si disponible
 
 app.listen(PORT, () => {
-    console.log(`Servers running on : http://localhost:${PORT}`)
-})
+    console.log(`Server running on : http://localhost:${PORT}`);
+});
