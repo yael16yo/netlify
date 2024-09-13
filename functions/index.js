9205ef302b3b9c7febe7a1f42b49021af36abff1
yael16yo <75533817+yael16yo@users.noreply.github.com>
2024-09-13 21:39:16 +0200

    update(index: cors)

--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,17 @@ require('dotenv').config(); // Importer dotenv pour charger les variables d'envi
 
 const express = require("express");
 const app = express();
-const cors = require('cors');
+//const cors = require('cors');
 
 const routeMails = require('./routes/routeMails.js');
 
 // Configuration de CORS et JSON
-const corsOptions = {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content', 'Accept', 'Content-Type', 'Authorization']
-};
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 app.use(express.json());
 
 // Utiliser les routes
